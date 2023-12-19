63348149842fe9d4b4c0b650fc4e12bef61efb5e
Miguel Angel Chanchavac Alvarado <miguel.chanchavac2502@gmail.com>
2023-12-19 01:37:48 +0000

    new tables

---
+++ models/fyrTest/rel_caja_bancos.sql
@@ -0,0 +1,17 @@
+with rel_caja_bancos as (
+select 
+	posicion ,
+	cuenta_resumen  ,
+	to_date(to_char(fecha_operacion, 'yyyy-MM-')||'01', 'yyyy-MM-dd') as fecha_operacion,
+	sum(saldo_inicial) as saldo_inicial,
+	sum(debe_2) as debe,
+	sum(haber_2) as haber
+from stage.stage_ingreso_historia sih
+where 1=1 
+and posicion in ('1.8') --CAJA Y BANCOS
+group by posicion ,
+cuenta_resumen,
+to_date(to_char(fecha_operacion, 'yyyy-MM-')||'01', 'yyyy-MM-dd')
+order by fecha_operacion, cuenta_resumen 
+)
+select * from rel_caja_bancos
